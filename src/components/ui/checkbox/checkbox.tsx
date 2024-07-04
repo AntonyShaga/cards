3abe51a670e2c60514ec561b45ce0d4571eb284d
Anthony Shaga <antonyshaga@gmail.com>
2024-07-04 23:09:55 +0300

    first commit

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
+
+import { CheckBoxIcon } from '@/icon/checkBoxIcon/checkBoxIcon'
 import * as Checkbox from '@radix-ui/react-checkbox'
-import { CheckIcon } from '@radix-ui/react-icons'
 
 import s from './checkbox.module.scss'
+
 export const CheckBox = () => {
+  const [checked, setChecked] = useState(false)
+
   return (
     <div className={s.Checkbox}>
       <Checkbox.Root className={s.CheckboxRoot}>
         <Checkbox.Indicator className={s.ew}>
-          <CheckIcon className={s.CheckIcon} />
+          <CheckBoxIcon cheacked={checked} />
         </Checkbox.Indicator>
       </Checkbox.Root>
     </div>
