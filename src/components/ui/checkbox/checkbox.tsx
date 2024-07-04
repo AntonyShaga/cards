d3f30542ef5f0abe33f47324564d0f1b5e8226e2
Anthony Shaga <antonyshaga@gmail.com>
2024-07-05 00:02:54 +0300

    first commit

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 
 import { CheckBoxIcon } from '@/icon/checkBoxIcon/checkBoxIcon'
-import { CheckBoxIconRam } from '@/icon/checkBoxIcon/checkBoxIconRam.tsx'
+import { CheckBoxIconRam } from '@/icon/checkBoxIcon/checkBoxIconRam'
 import * as Checkbox from '@radix-ui/react-checkbox'
+import { CheckedState } from '@radix-ui/react-checkbox'
 
 import s from './checkbox.module.scss'
 
 export const CheckBox = () => {
-  const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState<CheckedState | undefined>('indeterminate')
+  const onClickHandler = () => {
+    setChecked(prevIsChecked => (prevIsChecked === 'indeterminate' ? true : 'indeterminate'))
+  }
 
   return (
     <div className={s.Checkbox}>
-      <Checkbox.Root className={s.CheckboxRoot}>
+      <Checkbox.Root checked={checked} className={s.CheckboxRoot} onCheckedChange={onClickHandler}>
         <Checkbox.Indicator className={s.ew}>
-          <CheckBoxIconRam />
-          {/*<CheckBoxIcon />*/}
+          {checked === 'indeterminate' && <CheckBoxIconRam />}
+          {checked === true && <CheckBoxIcon />}
         </Checkbox.Indicator>
       </Checkbox.Root>
     </div>
--- src/icon/checkBoxIcon/checkBoxIcon.tsx
+++ src/icon/checkBoxIcon/checkBoxIcon.tsx
@@ -7,24 +7,12 @@ export const CheckBoxIcon = () => {
       width={'18'}
       xmlns={'http://www.w3.org/2000/svg'}
     >
-      <rect fill={'#3D3D3D'} height={'18'} width={'18'} />
-      <rect fill={'red'} height={'382'} transform={'translate(-689 -182)'} width={'1721'} />
-      <rect
-        height={'73'}
-        rx={'4.5'}
-        stroke={'#9747FF'}
-        strokeDasharray={'10 5'}
-        width={'1624'}
-        x={'-667.5'}
-        y={'-27.5'}
-      />
-      <circle cx={'9'} cy={'9'} fill={'#333333'} r={'18'} />
-      <rect fill={'green'} height={'12'} width={'16'} x={'1'} y={'3'} />
+      <rect fill={'transparent'} height={'12'} width={'16'} x={'1'} y={'3'} />
       <path
         d={
           'M16 0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.11 18 18 17.1 18 16V2C18 0.9 17.11 0 16 0ZM7 14L2 9L3.41 7.59L7 11.17L14.59 3.58L16 5L7 14Z'
         }
-        fill={'red'}
+        fill={'#fff'}
       />
     </svg>
   )
